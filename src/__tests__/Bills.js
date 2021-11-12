f0cac36253bf73a6efcb56caaadbb97e50fa7c43
Mohamad Ghazal <mghazal1024@gmail.com>
2021-11-12 09:20:11 +0100

    employee icon highlight test added and passed

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -5,13 +5,23 @@
 import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
+import VerticalLayout from "../views/VerticalLayout"
+import { localStorageMock } from "../__mocks__/localStorage.js"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
-      document.body.innerHTML = html
+      // const html = BillsUI({ data: []})
+      // document.body.innerHTML = html
       //to-do write expect expression
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      const user = JSON.stringify({
+        type: 'Employee'
+      })
+      window.localStorage.setItem('user', user)
+      const html = VerticalLayout(120)
+      document.body.innerHTML = html
+      expect(screen.getByTestId('icon-window')).toBeTruthy()
     })
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
